82591e619c817c45a69ffaafe6dad1dad3036f7b
Eric Ito <eric.ito@gmail.com>
2014-06-21 22:25:04 -0700

    add files

---
+++ SwiftOnlyFeaturesFramework/SwiftOnlyFeaturesFramework/SwiftOnlyFeaturesFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOnlyFeaturesFramework.h
+//  SwiftOnlyFeaturesFramework
+//
+//  Created by Eric Ito on 6/21/14.
+//  Copyright (c) 2014 Eric Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOnlyFeaturesFramework.
+FOUNDATION_EXPORT double SwiftOnlyFeaturesFrameworkVersionNumber;
+
+//! Project version string for SwiftOnlyFeaturesFramework.
+FOUNDATION_EXPORT const unsigned char SwiftOnlyFeaturesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOnlyFeaturesFramework/PublicHeader.h>
+
+
